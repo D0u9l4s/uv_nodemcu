ced36948a261e102590c6b990bd85f8fcaef3859
Douglas <doug.ag@hotmail.com>
2021-11-21 13:33:17 -0300

    Correção comentários.

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@ Adafruit_VEML6070 uv = Adafruit_VEML6070();
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("VEML6070 Test");
-  uv.begin(VEML6070_1_T); // pass in the integration time constant
+  Serial.println("VEML6070");
+  uv.begin(VEML6070_1_T);
 }
 
 void loop()
